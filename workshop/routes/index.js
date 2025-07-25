bc99df42903ac5612fe538e7a86be39b1828f856
Nildson Bezerra <nildsonb15@gmail.com>
2025-07-25 20:47:58 -0300

    Atualização da tela incial de login, js, ejs e css

--- workshop/routes/index.js
+++ workshop/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
 });
 //Página para cadastro de pacientes
 router.get('/cadastro', (req, res) => {
