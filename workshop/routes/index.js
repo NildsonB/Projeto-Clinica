7a4d22920b14111144e59d1ab3045ce246329180
Nildson <nildsonb15@gmail.com>
2025-07-25 21:22:28 -0300

    Adição de rota post para cadastro de usuário

--- workshop/routes/index.js
+++ workshop/routes/index.js
@@ -9,11 +9,22 @@ router.get('/', (req, res, next) => {
 router.get('/cadastro', (req, res) => {
   res.render('cadastro', {title: 'Cadastro de Paciente', action:""})
 })
-
+router.post('/cadastro', (req, resp) => {
+  const nome = req.body.nome;
+  const idade = !req.body.nascimento?null:req.body.nascimento;
+  const fone = parseInt(req.body.telefone);
+  const endereco = req.body.endereco;
+  try {
+    //Parte que receberá a incorporação com BDS
+  } catch (error) {
+    res.redireci('/erro=' + error);
+  }
+})
+//Página de cadastro de médicos
 router.get('/cadastromed', (req, res) => {
   res.render('cadastromed', {title: 'Cadastro de Medicos', action:""})
 })
-
+//Página de cadastro do plano de saúde
 router.get('/cadastroplano', (req, res) => {
   res.render('cadastroplano', {title: 'Cadastro de Plano de Saúde', action:""})
 })
